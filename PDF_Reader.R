83fb4e6b76a911ce2fe7f097d4033466591c75ba
Greg Pilgrim <gpilgrim2670@gmail.com>
2020-03-31 14:17:53 -0400

    Changed gender encoding in ePostal_Reader, added 2020 results to Postal.csv

--- PDF_Reader.R
+++ PDF_Reader.R
@@ -7,8 +7,7 @@ Womens <-
   pdf_text("inst/extdata/2020 OHEP Womens Preliminary Results.pdf")
 
 ePostal_Reader <- function(file_path) {
-  
-  Year = stringr::str_extract(file_path, "\\d{4}")
+  Year <- stringr::str_extract(file_path, "\\d{4}")
   
   file <- pdftools::pdf_text(file_path)
   
@@ -17,8 +16,8 @@ ePostal_Reader <- function(file_path) {
   
   row_numbs <- seq(1, length(as_lines_list_2), 1)
   as_lines_list_2 <- paste(as_lines_list_2, row_numbs, sep = "  ")
-
-  Gender = ifelse(any(stringr::str_detect(as_lines_list_2, "Men")), "M", "W")
+  
+  Gender = ifelse(any(stringr::str_detect(as_lines_list_2, "Men")), "M", "F")
   
   data_1 <- as_lines_list_2 %>%
     stringr::str_extract_all("\n\\s*\\d{1,3}.*") %>%
@@ -146,8 +145,10 @@ ePostal_Reader <- function(file_path) {
       as.data.frame(t(as.data.frame(data_length_7)),
                     row.names = FALSE,
                     stringsAsFactors = FALSE) %>%
-      dplyr::mutate(Club = stringr::str_split_fixed(V5, " ", n = 2)[, 1],
-             ID = stringr::str_split_fixed(V5, " ", n = 2)[, 2]) %>%
+      dplyr::mutate(
+        Club = stringr::str_split_fixed(V5, " ", n = 2)[, 1],
+        ID = stringr::str_split_fixed(V5, " ", n = 2)[, 2]
+      ) %>%
       dplyr::select(
         Place = V1,
         First_Name = V2,
@@ -181,7 +182,7 @@ ePostal_Reader <- function(file_path) {
                     row.names = FALSE,
                     stringsAsFactors = FALSE) %>%
       dplyr::select(ID_Cut = V1,
-             Row_Numb = V2)
+                    Row_Numb = V2)
     
   } else {
     df_2 <- data.frame(
@@ -204,19 +205,24 @@ ePostal_Reader <- function(file_path) {
     dplyr::full_join(df_7) %>%
     dplyr::mutate(Row_Numb = as.numeric(Row_Numb)) %>%
     dplyr::arrange(Row_Numb) %>%
-    dplyr::mutate(ID = dplyr::case_when(
-      stringr::str_detect(ID, "-") == FALSE & stringr::str_detect(ID, "[:lower:]") == FALSE ~ paste(df_2$ID_Cut, ID, sep = ""),
-      TRUE ~ ID
-    )) %>%
+    dplyr::mutate(
+      ID = dplyr::case_when(
+        stringr::str_detect(ID, "-") == FALSE &
+          stringr::str_detect(ID, "[:lower:]") == FALSE ~ paste(df_2$ID_Cut, ID, sep = ""),
+        TRUE ~ ID
+      )
+    ) %>%
     tidyr::fill(Age_Group, .direction = "down") %>%
     dplyr::mutate(National_Record = dplyr::case_when(is.na(National_Record) == TRUE ~ "N",
-                                       TRUE ~ National_Record)) %>% 
-    dplyr::mutate(Place = as.numeric(Place),
-           Age = as.numeric(Age),
-           Distance = stringr::str_replace(Distance, ",", ""),
-           Distance = as.numeric(Distance),
-           Gender = Gender,
-           Year = as.numeric(Year)) %>% 
+                                                     TRUE ~ National_Record)) %>%
+    dplyr::mutate(
+      Place = as.numeric(Place),
+      Age = as.numeric(Age),
+      Distance = stringr::str_replace(Distance, ",", ""),
+      Distance = as.numeric(Distance),
+      Gender = Gender,
+      Year = as.numeric(Year)
+    ) %>%
     dplyr::select(-Row_Numb)
   
   return(data)
@@ -226,3 +232,4 @@ Mens_2020 <- ePostal_Reader("inst/extdata/2020 OHEP Mens Preliminary Results.pdf
 Womens_2020 <- ePostal_Reader("inst/extdata/2020 OHEP Womens Preliminary Results.pdf")
 
 df_2020 <- bind_rows(Mens_2020, Womens_2020)
+write.csv(df_2020, row.names = FALSE, "Postal_2020.csv")
